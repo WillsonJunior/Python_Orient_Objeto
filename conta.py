d01c6eb54aaa4b388f6fb7c7afd0ecdffeb15292
Wilson Junior <76814978+WillsonJunior@users.noreply.github.com>
2023-07-12 14:26:46 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -1,17 +1,22 @@
 class Conta:
-  def __init__(self, numero, titular, saldo, limite):
-    self.__numero= numero
-    self.__titular= titular
-    self.__saldo= saldo
-    self.__limite= limite
 
-  def extrato(self):
-    print("saldo de {} do titular {}".format(self.saldo, self.titular))
-  
-  def deposita(self,valor):
-    self.saldo += valor
-  def saca(self,valor):
-    self.saldo -= valor
-def trans(self, valor, origem, destino):
-  origem.sacar(valor)
-  destino.deposita(valor)
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ...{}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.__saldo,self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
