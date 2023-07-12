00792616012e4f8df641d5a0c335c7ebcf5f3c53
Wilson Junior <76814978+WillsonJunior@users.noreply.github.com>
2023-07-12 09:33:53 -0300

    Create conta.py

---
+++ conta.py
@@ -0,0 +1,14 @@
+class Conta:
+  def __init__(self, numero, titular, saldo, limite):
+    self.__numero= numero
+    self.__titular= titular
+    self.__saldo= saldo
+    self.__limite= limite
+
+  def extrato(self):
+    print("saldo de {} do titular {}".format(self.saldo, self.titular))
+  
+  def deposita(self,valor):
+    self.saldo += valor
+  def saca(self,valor):
+    self.saldo -= valor
