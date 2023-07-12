b9f5c2f173ecbd9dbcf40f070fb7f55dcebf1a2b
Wilson Junior <76814978+WillsonJunior@users.noreply.github.com>
2023-07-12 15:53:36 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -10,7 +10,11 @@ conta1 = Conta(789456, "Pietro", 90.0 , 1000.0)
 
 conta2 = Conta(57823, "maria lucia", 90.0 , 1000.0)
 
-#usando a função de tranferênc/ia
-conta1.transfere(10, conta2)
-conta1.extrato()
-conta2.extrato()
+#testando o mtodo de tranferência
+#conta1.transfere(10, conta2)
+#conta1.extrato()
+#conta2.extrato()
+
+#testando os metodos criado get e set
+
+conta.get_saldo()
